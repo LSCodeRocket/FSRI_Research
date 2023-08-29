fe00bcbb4a2ebdc9bdc289f16c6e5ed1e80771f4
Logan Smith-Perkins <logansmithp23@gmail.com>
2023-08-29 13:38:06 -0700

    Made 100 Surface Curves

--- visualization_neural_network.py
+++ visualization_neural_network.py
@@ -8,34 +8,96 @@ from progress.bar import Bar
 
 creep_dict, surface_dict = folder_to_dictionaries(sys.argv[1])
 
-keys = list(creep_dict.keys())
+def display_creep_curves():
 
-bar = Bar("Making Curves", max=int(sys.argv[2]))
+    keys = list(creep_dict.keys())
 
-for i in range(int(sys.argv[2])):
+    bar = Bar("Making Curves", max=int(sys.argv[2]))
 
-    random_curve_id = random.randint(0, len(keys))
-    creep_curve = creep_dict[keys[random_curve_id]]
+    for i in range(int(sys.argv[2])):
 
-    input_min = min(creep_curve[0])
-    input_max = max(creep_curve[0])
+        random_curve_id = random.randint(0, len(keys))
+        creep_curve = creep_dict[keys[random_curve_id]]
 
-    positive_input = np.linspace(input_min, input_max, num=len(creep_curve[0]))
-    negative_input = list(-positive_input)
-    negative_input.reverse()
+        input_min = min(creep_curve[0])
+        input_max = max(creep_curve[0])
 
-    full_input = np.array(negative_input + list(positive_input))
+        positive_input = np.linspace(input_min, input_max, num=len(creep_curve[0]))
+        negative_input = list(-positive_input)
+        negative_input.reverse()
 
-    positive_x_output = list(creep_curve[1])
-    negative_x_output = positive_x_output
-    negative_x_output.reverse()
+        full_input = np.array(negative_input + list(positive_input))
 
-    full_output = np.array(negative_x_output + positive_x_output)
+        positive_x_output = list(creep_curve[1])
+        negative_x_output = positive_x_output
+        negative_x_output.reverse()
 
-    plt.clf()
-    plt.title(f"{keys[random_curve_id]}")
-    plt.plot(positive_input, positive_x_output)
-    plt.savefig(f"creep_curves/{keys[random_curve_id][:-4]}.png")
-    bar.next()
+        full_output = np.array(negative_x_output + positive_x_output)
 
-bar.finish()
+        plt.clf()
+        plt.title(f"{keys[random_curve_id]}")
+        plt.plot(positive_input, positive_x_output)
+        plt.savefig(f"creep_curves/{keys[random_curve_id][:-4]}.png")
+        bar.next()
+    bar.finish()
+
+
+
+def display_surface_curves():
+    model = torch.load("model_50nphl_4hl.pth")
+
+    keys = list(surface_dict.keys())
+
+    bar = Bar("Making Curves", max=int(sys.argv[2]))
+
+    input_min = find_maximum_minimum_input(creep_dict)
+    input_max = find_minimum_maximum_input(creep_dict)
+
+    for i in range(int(sys.argv[2])):
+
+        random_curve_id = random.randint(0, len(keys))
+        surface_curve = surface_dict[keys[random_curve_id]]
+
+        positive_input = list(np.logspace(0, np.log10(30), num=90))
+        negative_input = []
+        for value in reversed(positive_input):
+            negative_input.append(-value)
+
+        full_input = np.array(negative_input + list(positive_input))
+
+        positive_x_output = list(surface_curve[1])
+        negative_x_output = []
+        for value in reversed(positive_x_output):
+            negative_x_output.append(value)
+
+        full_output = np.array(negative_x_output + positive_x_output)
+
+        creep_input = creep_dict[keys[random_curve_id]][0]
+        creep_input_curve = creep_dict[keys[random_curve_id]][1]
+        
+        creep_fitted_coefficients = np.polyfit(creep_input, creep_input_curve, deg=polynomial_degree_approximation)
+
+        #make function for fitted curve
+        creep_func = lambda t: sum([ t**(polynomial_degree_approximation-n) * creep_fitted_coefficients[n] for n in range(polynomial_degree_approximation) ])
+
+        #evaluate new curve at specific points across all new curves
+        new_creep = [creep_func(x) for x in np.linspace(input_min, input_max, num=input_number)]
+
+
+        positive_x_model = list(model(torch.Tensor(new_creep)).detach().numpy())
+        negative_x_model = []
+        for value in reversed(positive_x_model):
+            negative_x_model.append(value)
+
+        full_model = np.array(negative_x_model + positive_x_model)
+
+        plt.clf()
+        plt.title(f"{keys[random_curve_id][:-4]}")
+        plt.plot(full_input, full_output)
+        plt.plot(full_input, full_model)
+        plt.savefig(f"surface_curves/{keys[random_curve_id][:-4]}.png")
+        bar.next()
+    bar.finish()
+
+
+display_surface_curves()
